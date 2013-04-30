f9b38fcca75fe27618143e5c963ebcfe4e281e06
Ivan Shakuta <ishakuta@magecore.com>
2013-04-30 19:29:43 +0300

    Contact bundle config added, composer repo added

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -46,6 +46,7 @@ class AppKernel extends Kernel
             new Oro\Bundle\GridBundle\OroGridBundle(),
             new Oro\Bundle\WindowsBundle\OroWindowsBundle(),
             new Oro\Bundle\AccountBundle\OroAccountBundle(),
+            new Oro\Bundle\ContactBundle\OroContactBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
