b79841abf78dbbe786438afe67efb3fccbc140bf
yurio <yurio@ua.fm>
2013-05-24 10:59:35 +0300

    BAP-847: Create devjs environment for BAP

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -54,7 +54,7 @@ class AppKernel extends Kernel
             new Acme\Bundle\DemoBundle\AcmeDemoBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), array('dev', 'devjs', 'test'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
