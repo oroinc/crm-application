d56975ac74bd679f3181e3b8937f673565557731
Roman Grebenchuk <rgrebenchuk@magecore.com>
2013-05-27 22:12:59 +0300

    Initial repository

---
+++ install.bat
@@ -0,0 +1,8 @@
+php app/console doctrine:database:create
+php app/console doctrine:schema:create
+php app/console oro:search:create-index
+php app/console doctrine:fixture:load --no-debug --no-interaction
+php app/console oro:acl:load
+php app/console oro:navigation:init
+php app/console assets:install web
+php app/console assetic:dump
