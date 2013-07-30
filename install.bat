cb4e871c09855cceb2c79d9f43bfdf747a1a4f21
Alexander Nezdoiminoga <alex@magecore.com>
2013-07-30 17:11:20 +0300

    update install scripts

--- install.bat
+++ install.bat
@@ -8,6 +8,7 @@ php app/console oro:search:create-index --env %ENV%
 php app/console doctrine:fixture:load --no-debug --no-interaction --env %ENV%
 php app/console oro:acl:load --env %ENV%
 php app/console oro:navigation:init --env %ENV%
+php app/console oro:entity-config:update --env %ENV%
 php app/console assets:install web --env %ENV%
 php app/console assetic:dump --env %ENV%
 php app/console oro:assetic:dump
