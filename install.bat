a1325ca78203e07b5647881a35a07074193df9f5
Alexander Nezdoiminoga <alex@magecore.com>
2013-08-27 21:16:49 +0300

    Merge BAP-1132 into master

--- install.bat
+++ install.bat
@@ -9,6 +9,7 @@ php app/console doctrine:fixture:load --no-debug --no-interaction --env %ENV% ||
 php app/console oro:acl:load --env %ENV% || goto :error
 php app/console oro:navigation:init --env %ENV% || goto :error
 php app/console oro:entity-config:update --env %ENV% || goto :error
+php app/console oro:entity-extend:create --env %ENV% || goto :error
 php app/console assets:install web --env %ENV% || goto :error
 php app/console assetic:dump --env %ENV% || goto :error
 php app/console oro:assetic:dump || goto :error
--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ php app/console doctrine:fixture:load --no-debug --no-interaction --env $ENV
 php app/console oro:acl:load --env $ENV
 php app/console oro:navigation:init --env $ENV
 php app/console oro:entity-config:update --env $ENV
+php app/console oro:entity-extend:create --env $ENV
 php app/console assets:install web --env $ENV
 php app/console assetic:dump --env $ENV
 php app/console oro:assetic:dump
