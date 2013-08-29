ead8626e49ecad41a32127017c2e16721da28914
Roman Grebenchuk <rgrebenchuk@magecore.com>
2013-08-29 20:27:14 +0300

    Update install scripts

--- install.bat
+++ install.bat
@@ -4,15 +4,17 @@ if "%1" NEQ "" (
     set ENV=%1
 )
 php app/console doctrine:schema:create --env %ENV% || goto :error
-php app/console oro:search:create-index --env %ENV% || goto :error
 php app/console doctrine:fixture:load --no-debug --no-interaction --env %ENV% || goto :error
 php app/console oro:acl:load --env %ENV% || goto :error
 php app/console oro:navigation:init --env %ENV% || goto :error
 php app/console oro:entity-config:update --env %ENV% || goto :error
 php app/console oro:entity-extend:create --env %ENV% || goto :error
+php app/console cache:clear --env %ENV% || goto :error
+php app/console doctrine:schema:update --force --env %ENV% || goto :error
+php app/console oro:search:create-index --env %ENV% || goto :error
 php app/console assets:install web --env %ENV% || goto :error
 php app/console assetic:dump --env %ENV% || goto :error
-php app/console oro:assetic:dump || goto :error
+php app/console oro:assetic:dump --env %ENV% || goto :error
 goto :EOF
 
 :error
