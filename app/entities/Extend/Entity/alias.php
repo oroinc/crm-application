6980589547a32cd0b9bbea292fd57204d46dfa8d
Falko Konstantin <kotfalya@gmail.com>
2013-09-12 15:15:00 +0300

    fix aliases

--- app/entities/Extend/Entity/alias.php
+++ app/entities/Extend/Entity/alias.php
@@ -1,11 +1,14 @@
 <?php
 
-$aliases = \Symfony\Component\Yaml\Yaml::parse(file_get_contents(__DIR__ . '/alias.yml'));
+$aliasPath = __DIR__ . '/alias.yml';
+if (file_exists($aliasPath)) {
+    $aliases = \Symfony\Component\Yaml\Yaml::parse(file_get_contents(__DIR__ . '/alias.yml', FILE_USE_INCLUDE_PATH));
 
-if (is_array($aliases)) {
-    foreach ($aliases as $className => $alias) {
-        if (class_exists($className)) {
-            class_alias($className, $alias);
+    if (is_array($aliases)) {
+        foreach ($aliases as $className => $alias) {
+            if (class_exists($className)) {
+                class_alias($className, $alias);
+            }
         }
     }
 }
