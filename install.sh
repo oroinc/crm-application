fa92bc2c95450193fc94c3cb6fa504a511dfd749
kotfalya <kotfalya@gmail.com>
2013-09-23 00:34:17 +0300

    extend, some commit

--- install.sh
+++ install.sh
@@ -6,11 +6,11 @@ then
 fi
 php app/console oro:entity-extend:clear --env $ENV
 php app/console doctrine:schema:create --env $ENV
+php app/console doctrine:fixture:load --no-debug --no-interaction --env $ENV
 php app/console oro:entity-config:update --env $ENV
 php app/console oro:entity-extend:load --env $ENV
 php app/console oro:entity-extend:dump --env $ENV
 php app/console doctrine:schema:update --env $ENV --force
-php app/console doctrine:fixture:load --no-debug --no-interaction --env $ENV
 php app/console oro:acl:load --env $ENV
 php app/console oro:navigation:init --env $ENV
 php app/console oro:search:create-index --env $ENV
