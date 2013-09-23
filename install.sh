0ad7605f5ae3a13743a3aa7f8e283d26863d0c6a
Falko Konstantin <kotfalya@gmail.com>
2013-09-23 22:09:52 +0300

    generator refactoring

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ fi
 php app/console oro:entity-extend:clear --env $ENV
 php app/console doctrine:schema:create --env $ENV
 php app/console doctrine:fixture:load --no-debug --no-interaction --env $ENV
-php app/console oro:entity-config:update --env $ENV
-php app/console oro:entity-extend:load --env $ENV
-php app/console oro:entity-extend:dump --env $ENV
+php app/console oro:entity-config:init --env $ENV
+php app/console oro:entity-extend:init --env $ENV
+php app/console oro:entity-extend:update-config --env $ENV
 php app/console doctrine:schema:update --env $ENV --force
 php app/console oro:acl:load --env $ENV
 php app/console oro:navigation:init --env $ENV
