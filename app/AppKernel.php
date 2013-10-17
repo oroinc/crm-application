239961c59f47c5590cd796fbd908e6c2384adb18
Dan Yasnyuk <danil@magecore.com>
2013-10-17 13:54:42 +0300

    BAP-1787: Automatic bundles distribution for application - AppKernel.php updated

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -3,50 +3,15 @@
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
-class AppKernel extends Kernel
+use Oro\Bundle\DistributionBundle\OroKernel;
+
+class AppKernel extends OroKernel
 {
     public function registerBundles()
     {
         $bundles = array(
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            //new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            new JMS\JobQueueBundle\JMSJobQueueBundle(),
-            new JMS\SerializerBundle\JMSSerializerBundle($this),
-            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
-            new FOS\RestBundle\FOSRestBundle(),
-            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
-            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
-            new BeSimple\SoapBundle\BeSimpleSoapBundle(),
-            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-            new Escape\WSSEAuthenticationBundle\EscapeWSSEAuthenticationBundle(),
-            new Liip\ImagineBundle\LiipImagineBundle(),
-            new Bazinga\ExposeTranslationBundle\BazingaExposeTranslationBundle(),
-            new APY\JsFormValidationBundle\APYJsFormValidationBundle(),
-            new Genemu\Bundle\FormBundle\GenemuFormBundle(),
-            new A2lix\TranslationFormBundle\A2lixTranslationFormBundle(),
-            new JDare\ClankBundle\JDareClankBundle(),
-            new Lexik\Bundle\MaintenanceBundle\LexikMaintenanceBundle(),
-            new Sylius\Bundle\FlowBundle\SyliusFlowBundle(),
-
             // BAPP bundles
             new OroPro\Bundle\EwsBundle\OroProfessionalEwsBundle(),
-
-            // CRM bundles
-            new OroCRM\Bundle\AccountBundle\OroCRMAccountBundle(),
-            new OroCRM\Bundle\ContactBundle\OroCRMContactBundle(),
-            new OroCRM\Bundle\DashboardBundle\OroCRMDashboardBundle(),
-            new OroCRM\Bundle\SalesBundle\OroCRMSalesBundle(),
-            new OroCRM\Bundle\ReportBundle\OroCRMReportBundle(),
-            new OroCRM\Bundle\DemoDataBundle\OroCRMDemoDataBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
@@ -59,10 +24,7 @@ class AppKernel extends Kernel
             $bundles[] = new Oro\Bundle\TestFrameworkBundle\OroTestFrameworkBundle();
         }
 
-        // BAP bundles
-        $bundles = array_merge($bundles, Oro\Bundle\PlatformBundle\OroPlatformBundle::registeredBundles($this));
-
-        return $bundles;
+        return array_merge(parent::registerBundles(), $bundles);
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader)
