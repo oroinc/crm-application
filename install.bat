0fa5e82ef871a26c3c834a7ba88c9e1a8aae6965
Dmitry Khrysev <dkhrysev@magecore.com>
2013-10-18 18:52:38 +0300

    BAP-1894: JS formatters - oro:localization:dump added to installs

--- install.bat
+++ install.bat
@@ -15,6 +15,7 @@ php app/console oro:entity-extend:init --env %ENV% || goto :error
 php app/console oro:entity-extend:update-config --env %ENV% || goto :error
 php app/console doctrine:schema:update --env %ENV% --force || goto :error
 php app/console oro:search:create-index --env %ENV% || goto :error
+php app/console oro:localization:dump --env $ENV || goto :error
 php app/console assets:install web --env %ENV% || goto :error
 php app/console assetic:dump --env %ENV% || goto :error
 php app/console oro:assetic:dump --env %ENV% || goto :error
--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ php app/console oro:entity-extend:init --env $ENV
 php app/console oro:entity-extend:update-config --env $ENV
 php app/console doctrine:schema:update --env $ENV --force
 php app/console oro:search:create-index --env $ENV
+php app/console oro:localization:dump --env $ENV
 php app/console assets:install web --env $ENV
 php app/console assetic:dump --env $ENV
 php app/console oro:assetic:dump --env $ENV
