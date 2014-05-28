aeccd9bc8ce3338dc7e9c4540b47c89e142f9752
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2014-05-28 18:28:47 +0300

    OC-251: Check PhpExecutableFinder Version - use shell_exec instead of process to avoid cache warmup
    Signed-off-by: Sergey Zhuravel <zhuravel.sergey@gmail.com>

--- app/OroRequirements.php
+++ app/OroRequirements.php
@@ -5,6 +5,8 @@ require_once __DIR__ . '/SymfonyRequirements.php';
 use Symfony\Component\Process\ProcessBuilder;
 use Symfony\Component\Intl\Intl;
 
+use Oro\Bundle\InstallerBundle\Process\PhpExecutableFinder;
+
 /**
  * This class specifies all requirements and optional recommendations that are necessary to run the Oro Application.
  */
@@ -363,22 +365,14 @@ class OroRequirements extends SymfonyRequirements
      */
     protected function checkCliRequirements()
     {
-        if (class_exists('\Oro\Bundle\InstallerBundle\Process\PhpExecutableFinder')) {
-            $finder  = new \Oro\Bundle\InstallerBundle\Process\PhpExecutableFinder();
-            $command = sprintf(
-                '%s %sconsole oro:platform:check-requirements --env=%s',
-                $phpExec = $finder->find(),
-                __DIR__ . DIRECTORY_SEPARATOR,
-                'prod'
-            );
+        $finder  = new PhpExecutableFinder();
+        $command = sprintf(
+            '%s %soro-check.php',
+            $finder->find(),
+            __DIR__ . DIRECTORY_SEPARATOR
+        );
 
-            $process = new \Symfony\Component\Process\Process($command);
-            $process->run();
-
-            return $process->getOutput();
-        }
-
-        return null;
+        return shell_exec($command);
     }
 }
 
