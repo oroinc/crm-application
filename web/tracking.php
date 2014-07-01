ff61c2369f8ff903dec0bb119809a53f187d1307
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2014-07-01 18:39:51 +0300

    CRM-1479: Create tracking handler (web/tracking.php)

---
+++ web/tracking.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * This is tracking endpoint, which must be as fast as possible.
+ * KISS.
+ */
+
+$trackingFolder = '../app/logs/tracking';
+$settingsFile = $trackingFolder . DIRECTORY_SEPARATOR . 'settings.ser';
+$settings = array(
+    'dynamic_tracking_enabled' => false,
+    'dynamic_tracking_endpoint' => null,
+    'log_rotate_interval' => 60,
+    'piwik_host' => null,
+    'piwik_token_auth' => null
+);
+
+/**
+ * Pass request to given URL.
+ *
+ * @param string $url
+ */
+function passDataToUrl($url)
+{
+    if (strpos($url, 'http') !== 0) {
+        $url = 'http://' . $_SERVER['HTTP_HOST'] . $url;
+    }
+    $delimiter = strpos($url, '?') === false ? '?' : '&';
+    $url .= $delimiter . $_SERVER['QUERY_STRING'];
+
+    $handle = curl_init();
+    curl_setopt($handle, CURLOPT_URL, $url);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
+    curl_exec($handle);
+    curl_close($handle);
+}
+
+// Ensure tracking directory exists and read settings
+if (is_dir($trackingFolder)) {
+    if (is_readable($settingsFile)) {
+        $settings = unserialize(file_get_contents($settingsFile));
+    }
+} else {
+    mkdir($trackingFolder);
+}
+
+// Track visit
+if ($settings['dynamic_tracking_enabled']) {
+    // Pass visit to dynamic tracking endpoint
+    passDataToUrl($settings['dynamic_tracking_endpoint']);
+} else {
+    // Calculate interval part
+    $rotateInterval = 60;
+    $currentPart = 1;
+    if ($settings['log_rotate_interval'] > 0 && $settings['log_rotate_interval'] < 60) {
+        $rotateInterval = (int) $settings['log_rotate_interval'];
+        $passingMinute = intval(date('i')) + 1;
+        $currentPart = ceil($passingMinute / $rotateInterval);
+    }
+
+    // Construct file name
+    $fileName = date('Ymd-H') . '-' . $rotateInterval . '-' . $currentPart . '.log';
+
+    // Add visit to log to file
+    $data = json_encode($_GET) . PHP_EOL;
+    $fh = fopen($trackingFolder . DIRECTORY_SEPARATOR . $fileName, 'a');
+    if (flock($fh, LOCK_EX)) {
+        fwrite($fh, $data);
+        fflush($fh);
+        flock($fh, LOCK_UN);
+    }
+    fclose($fh);
+}
+
+// Pass tracking request to piwik instance
+if ($settings['piwik_host']) {
+    $piwikTrackingUrl = $settings['piwik_host'] . '/piwik.php';
+    if ($settings['piwik_token_auth']) {
+        $piwikTrackingUrl .= '?token_auth=' . $settings['piwik_token_auth'];
+    }
+
+    passDataToUrl($piwikTrackingUrl);
+}
+
+//Send 1x1 blank gif
+header('Content-Type: image/gif');
+echo base64_decode('R0lGODlhAQABAJAAAP8AAAAAACH5BAUQAAAALAAAAAABAAEAAAICBAEAOw==');
