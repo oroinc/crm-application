fc8f0d98464c3a247b9e23eb318b9cbc46b88994
Dincho Todorov <dincho@users.noreply.github.com>
2016-04-11 16:03:43 +0300

    Front controllers IP access restrictions
    - Disable IP access restrictions in CRM and CRM enterprise
    development front controllers

--- web/app_dev.php
+++ web/app_dev.php
@@ -12,13 +12,14 @@ use Symfony\Component\HttpFoundation\Request;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
+/*if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
+*/
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
