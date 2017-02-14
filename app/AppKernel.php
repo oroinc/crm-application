eaf2384524b2652f10c29e75fa8532ed737acc4d
Sergey Zhuravel <sergeyz@users.noreply.github.com>
2017-02-14 12:37:21 +0200

    OPA-5: Warmup all caches before functional tests execution (#7283)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -32,33 +32,4 @@ class AppKernel extends OroKernel
     {
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
     }
-
-    protected function initializeContainer()
-    {
-        static $first = true;
-
-        if ('test' !== $this->getEnvironment()) {
-            parent::initializeContainer();
-            return;
-        }
-
-        $debug = $this->debug;
-
-        if (!$first) {
-            // disable debug mode on all but the first initialization
-            $this->debug = false;
-        }
-
-        // will not work with --process-isolation
-        $first = false;
-
-        try {
-            parent::initializeContainer();
-        } catch (\Exception $e) {
-            $this->debug = $debug;
-            throw $e;
-        }
-
-        $this->debug = $debug;
-    }
 }
