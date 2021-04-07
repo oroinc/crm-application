47cb5c1e26043aecefd617f06cde679aabb7d64b
Dmitry Khrysev <x86demon@users.noreply.github.com>
2021-04-07 19:58:06 +0300

    BAP-20479: Fix failing weekly build pricing tests related to temporary tables (#29368)
    - added MySqlTempTableManipulator, made use of it in MergePricesCombiningStrategy
    - fixed MySQL fulltext search related tests and MySQL uuid related tests
    - fixed tests related to MySQL filtering differences
    - fixed MySQL privileges checks
    - fixed RestrictContentVariantByCategoryEventListenerTest test for MySQL

--- var/OroRequirements.php
+++ var/OroRequirements.php
@@ -275,7 +275,10 @@ class OroRequirements extends SymfonyRequirements
             $config = $this->getParameters($configYmlPath);
             $pdo = $this->getDatabaseConnection($config);
             if ($pdo) {
-                $requiredPrivileges = ['INSERT', 'SELECT', 'UPDATE', 'DELETE', 'TRUNCATE', 'REFERENCES', 'TRIGGER', 'CREATE', 'DROP'];
+                $requiredPrivileges = ['INSERT', 'SELECT', 'UPDATE', 'DELETE', 'REFERENCES', 'TRIGGER', 'CREATE', 'DROP'];
+                if ($pdo->getAttribute(PDO::ATTR_DRIVER_NAME) !== 'mysql') {
+                    $requiredPrivileges[] = 'TRUNCATE';
+                }
                 $notGrantedPrivileges = $this->getNotGrantedPrivileges($pdo, $requiredPrivileges, $config);
                 $this->addOroRequirement(
                     empty($notGrantedPrivileges),
